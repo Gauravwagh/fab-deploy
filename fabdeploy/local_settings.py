e6ce6f05e1ae8b11ecdcd9857f518a53da645913
Gauravwagh <gauravwagh11@gmail.com>
2016-04-14 16:10:58 +0530

    initial commit 1

--- fabdeploy/local_settings.py
+++ fabdeploy/local_settings.py
@@ -1,24 +1,18 @@
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+PROJECT_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = "a+-7nwmcbue*2l+5j4r6r4$c4d3bo0kn3ddapr7x)wt$c1%uv8"
-NEVERCACHE_KEY = "892b72cc-53b7-418f-987b-97246cb946a7b7ab61d8-940b-4b90-b0e9-eebe99b3e27007f39f4d-7dd3-4230-a923-798d5cf228eb"
+SECRET_KEY = '^*4supq(6-4z53-cwy%czely13k_)t9(43g9ba^*qqstf9z&n8'
+NEVERCACHE_KEY = "892b72cc-53b7-418f-987b-97246csddb946a7b7addsadasvvxb61d8-940b-4b90-b0e9-eebefdds99b3e27007f39f4d-7dd3-4230-a923-798d5cf228eb"
 
-
-
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "tenant_schemas.postgresql_backend",
-#         "NAME": "ckiller",
-#         "USER": "dbuser",
-#         "PASSWORD": "consultadd",
-#         "HOST": "localhost",
-#         "PORT": "",
-#
-#     }
-# }
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_ROOT_DIR, 'db.sqlite3'),
+    }
+}
 
 import os
 # Full filesystem path to the project.
@@ -31,19 +25,19 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__ + "/../"))
 
 
 FABRIC = {
-    "SSH_USER": "root", # SSH username
-    "SSH_PASS":  "Taulabs@2016", # SSH password (consider key-based authentication)
+    "SSH_USER": "user", # SSH username
+    "SSH_PASS":  "******", # SSH password (consider key-based authentication)
 #    "SSH_KEY_PATH":  "", # Local path to SSH key file, for key-based auth
-    "HOSTS": ['128.199.112.178'], # List of hosts to deploy to
-    "DOMAINS": ['ck.taulabs.co'], # Domains for public site
+    "HOSTS": ['122.265.852.25'], # List of hosts to deploy to
+    "DOMAINS": ['xyz.com'], # Domains for public site
     "VIRTUALENV_HOME":  "/var/www", # Absolute remote path for virtualenvs
-    "PROJECT_NAME": "ckiller_api", # Unique identifier for project
+    "PROJECT_NAME": "fabdeploy", # Unique identifier for project
     "REQUIREMENTS_PATH": "requirements.txt", # Path to pip requirements, relative to project
     "GUNICORN_PORT": 8001, # Port gunicorn will listen on
     "LOCALE": "en_US.UTF-8", # Should end with ".UTF-8"
-    "REPO_URL": "https://gitlab.com/consultadd_inc/ckiller.git", # Git or Mercurial remote repo URL for the project
-    "DB_PASS": "ckiller", # Live database password
-    "ADMIN_PASS": "ckiller505", # Live admin user password
+    "REPO_URL": "https://github.com/Gauravwagh/fab-deploy.git", # Git or Mercurial remote repo URL for the project
+    "DB_PASS": "dbpassword", # Live database password
+    "ADMIN_PASS": "adminpassword", # Live admin user password
     "SECRET_KEY": SECRET_KEY,
     "NEVERCACHE_KEY": NEVERCACHE_KEY,
     "APP_REPO_URL" : ""
--- fabdeploy/settings.py
+++ fabdeploy/settings.py
@@ -15,12 +15,17 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+try:
+    from local_settings import *
+except ImportError as e:
+    if "local_settings" not in str(e):
+        raise e
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^*4supq(6-4z53-cwy%czely13k_)t9(43g9ba^*qqstf9z&n8'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,12 +79,7 @@ WSGI_APPLICATION = 'fabdeploy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 # Password validation
